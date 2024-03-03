0da4a745321d171d2cc6b31c67e910126b296b8b
virgilio macero <virgiliomacero@gmail.com>
2024-03-03 15:53:53 -0500

    added typescript to react

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,38 @@
 import Logo from "./Logo.tsx";
 import PageMenu from "./PageMenu.tsx";
-import {useEffect} from "react"
+import { useEffect } from "react";
 
 function Header() {
-
-  useEffect(()=>{
-
+  useEffect(() => {
+    // @ts-ignore
     M.AutoInit();
-
-  },[])
+  }, []);
 
   var elem = document.querySelector(".sidenav");
+  // @ts-ignore
   var instance = M.Sidenav.init(elem);
 
   return (
     <>
-    <div className="navbar-fixed">
-      <nav className="white ">
-        <div className="nav-wrapper">
-          <div className="container">
-            <a href="#" data-target="mobile" className=" right sidenav-trigger">
-              <i className="material-icons" style={{ color: "black" }}>
-                menu
-              </i>
-            </a>
-            <Logo />
-            <PageMenu />
+      <div className="navbar-fixed">
+        <nav className="white ">
+          <div className="nav-wrapper">
+            <div className="container">
+              <a
+                href="#"
+                data-target="mobile"
+                className=" right sidenav-trigger"
+              >
+                <i className="material-icons" style={{ color: "black" }}>
+                  menu
+                </i>
+              </a>
+              <Logo />
+              <PageMenu />
+            </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
       <ul className="sidenav" id="mobile">
         <li>
           <a href="#Inicio">Inicio</a>
@@ -48,7 +51,6 @@ function Header() {
           <a href="#contacto">Contacto</a>
         </li>
       </ul>
-    
     </>
   );
 }
